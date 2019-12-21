c731377eeea360dcccee4891bd33405336316e04
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2019-12-20 20:23:31 -0500

    Update variable_logic.py

--- variables/logic/variable_logic.py
+++ variables/logic/variable_logic.py
@@ -4,6 +4,10 @@ def get_variables():
     queryset = Variable.objects.all()
     return (queryset)
 
+def get_variable(id):
+    variable = Variable.objects.raw("SELECT * FROM variables_variable WHERE id=%s" % id)
+    return (variable)
+
 def create_variable(form):
     measurement = form.save()
     measurement.save()
